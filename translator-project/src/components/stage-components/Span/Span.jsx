ac55523b6add21dd9820f267c8b546807034b0d5
Efaideleon <109036920+Efai-De-Leon@users.noreply.github.com>
2023-12-28 16:51:03 -0800

    refactored style

--- translator-project/src/components/stage-components/Span/Span.jsx
+++ translator-project/src/components/stage-components/Span/Span.jsx
@@ -5,22 +5,6 @@ export default function Span({ word, handleSpanWidthOverflowWithBreakline, id, d
     const spanRef = useRef(null)
     const [addBreakLine, setAddBreakLine] = useState(false)
     const [addedTheWords, setAddedTheWords] = useState(false)
-    const spanStyles = {
-        fontSize: "100px",
-        color: "white",
-        textAlign: "center",
-        lineHeight: "100%",
-        fontWeight: "900",
-        WebkitTextStrokeWidth: "4px",
-        TextStrokeWidth: "2px",
-        WebkitTextStrokeColor: "rgb(44, 44, 44)",
-        TextStrokeColor: "black",
-        WebkitFilter: "drop-shadow(0 5px 3px rgb(1, 1,1, 0.6))",
-        Filter: "drop-shadow(0 5px 3px rgb(1, 1,1, 0.6))",
-        letterspacing: "-5px",
-        wordSpacing: "12px",
-        padding: "0",
-    }
 
     useEffect(() => {
         if (!addedTheWords) {
@@ -42,7 +26,7 @@ export default function Span({ word, handleSpanWidthOverflowWithBreakline, id, d
 
     return (
         <span role={'span'}>
-            <span ref={spanRef} style={spanStyles} id="spans-text" >
+            <span ref={spanRef} className='span-text' id="spans-text" >
                 {addBreakLine ? <br data-test-id={'break'} /> : ""}
                 {word}
             </span>
